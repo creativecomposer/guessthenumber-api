c35fc8e134d81ecafab10ac4945da459a9832a0e
Creative Composer <a.jesudhason@hellobetter.de>
2023-05-26 13:48:35 +0200

    Remove prepare.js because husky conflicts with GitHub Actions

--- prepare.js
+++
@@ -1,8 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-if (!isProd) {
-  console.log('Installing husky');
-  require('husky').install('src/.husky');
-} else {
-  console.log('Environment is production. Do not install husky');
-}
