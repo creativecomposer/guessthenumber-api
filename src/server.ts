2bb56eee0a9382fdafd4877911cc87187a77d001
Creative Composer <a.jesudhason@hellobetter.de>
2023-05-29 10:43:49 +0200

    Update server.ts to run the server only in development mode

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,28 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import router from "./routes";
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.JWT_SECRET = process.env.JWT_SECRET || "ssshhhhhh";
 
 const app = express();
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function (_, res) {
+app.get("/", function (_, res) {
   res.status(200);
-  res.json({ message: 'Call one of the endpoints under /api/v1' });
+  res.json({ message: "Call one of the endpoints under /api/v1" });
 });
 
-if (process.env.NODE_ENV === 'development') {
-  app.listen(process.env.PORT || '3500', function () {
-    console.log('Server started');
+app.use("/api/v1", router);
+
+if (process.env.NODE_ENV === "development") {
+  app.listen(process.env.PORT || "3500", function () {
+    console.log("Server started");
   });
 } else {
   // At production mode, do not start server here. The script at ./bin/www will run prod server
